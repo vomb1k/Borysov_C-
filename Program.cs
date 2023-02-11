135df96418bf326d2c671f38bb5137a0e1f9d0b0
vomb1k <125087185+vomb1k@users.noreply.github.com>
2023-02-11 18:15:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace Borysov_.Net
+{
+    class TestTask
+    {
+        public static void DisplayMessage()
+        {
+            Console.Write("Введите любое целое число:  ");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+            if (inputNumber > 7)
+                Console.WriteLine("Привет");
+        }
+        public static void NameVerification()
+        {
+            Console.Write("Введите имя: ");
+            string inputName = Console.ReadLine();
+            if (inputName == "Вячеслав")
+                Console.WriteLine("Привет, {0}", inputName);
+            else Console.WriteLine("Такого имени нет");
+        }
+        public static void DisplayMultipleTo3Massive()
+        {
+            Console.Write("Введите размер массива: ");
+            int massiveSize = Convert.ToInt32(Console.ReadLine());
+            int[] massiveMultiple = new int[massiveSize];
+            Console.WriteLine("Заполните массив: ");
+            for(int i = 0; i < massiveSize; i++)
+                massiveMultiple[i] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Числа массива, кратные 3: ");
+            for(int i = 0; i < massiveMultiple.Length; i++)
+            {
+                if (massiveMultiple[i] % 3 == 0)
+                    Console.Write(" {0} ", massiveMultiple[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestTask.DisplayMessage();
+            TestTask.NameVerification();
+            TestTask.DisplayMultipleTo3Massive();
+        }
+    }
+}
